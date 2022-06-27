fc26f98772d3c93b4d053acff4f7830875dbdaaa
DanielaCortes25 <cortesriveradaniela@gmail.com>
2022-06-27 18:47:24 -0500

    Se modifica formulario de prestamo

--- resources/views/simulador.blade.php
+++ resources/views/simulador.blade.php
@@ -1,63 +1,71 @@
+@extends('layouts.app')
+@section('content')
+<!-- <form action="{{ route ('tablas')}}" method="POST">
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('assets/css/prestamos.css')}}">
-    <script src="{{asset('assets/js/prestamos.js')}}" defer></script>
-    <title>Simulador de Préstamos</title>
-</head>
-<body>
-    <div id="contenedor">
-        <div class="header">
-            <h2>Solicitud de préstamo</h2>
-        </div>
-        <div name="frmPrestamo" id="frmPrestamo">
-            <div class="control">
-                <label for="nombre">Nombre: <input type="text" name="nombre" id="nombre" placeholder="nombre"></label>
+                        {{ csrf_field() }}
+            <div class="modal-body">
+                <div class="form-group">
+                    <label for="category" >categoria</label>
+                    <input type="text" name="category" id="category">
+                </div>
             </div>
-            <div class="control">
-                <label for="fecha">Fecha: <input type="date" name="fecha" id="fecha" placeholder="fecha"></label>
+            <div>
+                <button type="submit" class="btn btn-dark"></button>
             </div>
-            <div class="control">
-                <label for="monto">Monto: <input type="number" name="monto" id="monto" placeholder="monto" min="500" step="500"></label>
+           
+        </form> -->
+<div class="container" id="registration-form">
+    <div class="image"></div>
+    <div class="frm">
+        <h3 id="h1-form">Ingrese los datos de su prestamo</h3>
+        <form action="{{ route ('tablas')}}" method="POST">
+        {{ csrf_field() }}
+            <div class="form-group">
+                <label for="nombre">Nombre:</label>
+                <input class="form-control" type="text" name="nombre" id="nombre" placeholder="nombre">
             </div>
-            <div class="control">
+            <div class="form-group">
+                <label for="fecha">Fecha:</label>
+                <input class="form-control"  type="date" name="fecha" id="fecha" placeholder="fecha">
+            </div>
+            <div class="form-group">
+                <label for="monto">Monto:</label>
+                <input class="form-control"  type="number" name="monto" id="monto" placeholder="monto" min="500" >
+            </div>
+            <div class="form-group">
+                <label for="periodo">Periodo:</label>
+                    <select class="form-control" name="periodo" id="periodo">
+                        <option class="form-control"  value="semanal">semanal</option>
+                        <option class="form-control" value="quincenal">quincenal</option>
+                        <option class="form-control" value="mensual">mensual</option>
+                    </select>
+                </label>
+            </div>
+            <div class="form-group">
+                <label for="interes">Interes:</label>
+                <input class="form-control" type="number" name="interes" id="interes" placeholder="interés" min="5" max="100" step=".01">
+            </div>
+            <div class="form-group">
+                <label for="plazo">Plazo:</label>
+                <input class="form-control"  type="number" name="plazo" id="plazo" min="1" max="120" step="1">
+            </div>
+
+
+            <div class="form-group">
+                <button type="submit" class="btn btn-success btn-lg">Simular</button>
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
+<!-- 
+     <div class="form-group">
                 <label for="periodo">Periodo:
-                    <select name="periodo" id="periodo">
+                    <select class="form-control" name="periodo" id="periodo">
                         <option value="semanal">semanal</option>
                         <option value="quincenal">quincenal</option>
                         <option value="mensual">mensual</option>
                     </select>
                 </label>
             </div>
-            <div class="control">
-                <label for="interes">Interés: <input type="number" name="interes" id="interes" placeholder="interés" min="5" max="100" step=".01"></label>
-            </div>
-            <div class="control">
-                <label for="plazo">Plazo (meses):
-                    <input type="number" name="plazo" id="plazo" min="1" max="12" step="1" required>
-                </label>
-            </div>
-
-            <button id="simular" onclick="simularPrestamo()">Simular préstamo</button>
-        </div>
-    </div>
-
-    <div id="contenedorTabla">
-        <div class="header">
-        <h2>Prestamo</h2>
-        </div>      
-        <div id="prestamo">     
-        </div>
-        <br>
-        <br>
-        <div class="header">
-        <h2>Amortizacion</h2>
-        </div>      
-        <div id="amortizaciones">     
-        </div>
-    </div>
-</body>
-</html>
+ -->
